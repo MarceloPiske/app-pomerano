f5ea621a223cb6fdac1ababf136be3e248c0bf2b
Piske <marcelopiske@ideyas.com>
2023-02-02 17:45:08 -0300

    feat: sw path adiconado

--- sw.js
+++ sw.js
@@ -1,32 +1,34 @@
-this.addEventListener('install', function (event) {
+const PATH = "app-pomerano"
+this.addEventListener("install", function (event) {
   event.waitUntil(
-    caches.open('v1').then(function (cache) {
+    caches.open("v1").then(function (cache) {
       return cache.addAll([
-        '../',
-        '../images/resized_images/',
-        '../index.html',
-        '../sobre.html',
-        '../styles/',
-        '../scripts/index.js',
-        '../categories.json',
-        '../sounds/'
+        `${PATH}/`,
+        `${PATH}/images/resized_images/`,
+        `${PATH}/index.html`,
+        `${PATH}/sobre.html`,
+        `${PATH}/styles/`,
+        `${PATH}/scripts/`,
+        `${PATH}/scripts/index.js`,
+        `${PATH}/categories.json`,
+        `${PATH}/sounds/`
       ]);
     })
   );
 });
 
-this.addEventListener('fetch', function(event) {
+this.addEventListener("fetch", function(event) {
   event.respondWith(
     caches.match(event.request).then(function(resp) {
       return resp || fetch(event.request).then(function(response) {
-        caches.open('v1').then(function(cache) {
+        caches.open("v1").then(function(cache) {
           cache.put(event.request, response.clone());
         });
         return response;
       });
     }).catch(function() {
       console.log("error cache");
-      return caches.match('/');
+      return caches.match("/");
     })
   );
 });
